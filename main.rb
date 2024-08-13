6651f3d1ac9ca76a0d783694f575e03f3e51dfd6
krypto <clayjeansen3@gmail.com>
2024-08-14 00:51:37 +0530

    update

--- main.rb
+++ main.rb
@@ -2,25 +2,29 @@ require_relative 'lib/hash'
 
 test = HashMap.new
 
-# test.set('apple', 'red')
-#  test.set('banana', 'yellow')
-#  test.set('carrot', 'orange')
- 
- test.set('elephant', 'gray')
- test.set('frog', 'green')
- test.set('grape', 'purple')
- test.set('hat', 'black')
- test.set('ice cream', 'white')
- test.set('jacket', 'blue')
- test.set('kite', 'pink')
- test.set('lion', 'golden')
 
-#  test.set('ice cream', 'pink')
-#  test.set('lion', 'king')
-#  test.set('jacket', 'black')
- test.set('java', 'spring boot')
- test.set('dog', 'brown')
+test.set('apple', 'red')
+test.set('banana', 'yellow')
+test.set('carrot', 'orange')
+test.set('dog', 'brown')
+test.set('elephant', 'gray')
+test.set('frog', 'green')
+test.set('grape', 'purple')
+test.set('hat', 'black')
+test.set('ice cream', 'white')
+test.set('jacket', 'blue')
+test.set('kite', 'pink')
+test.set('lion', 'golden')
 
- 
- puts test.length
- puts test.has?('grape')
+test.set('kite', 'red')
+test.set('dog', 'black')
+test.set('grape', 'black')
+test.set('lion', 'king')
+
+test.set('java', 'spring-boot')
+
+  
+
+
+p test.entries
+puts test.arr_length
